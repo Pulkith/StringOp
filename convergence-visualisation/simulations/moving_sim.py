3a692b56df206fec1b8200eac3ddefe0e4000f60
vincent@pangaea <vincent.von.appen@gmail.com>
2025-04-20 05:52:27 -0700

    Add moving targets to voronoi

---
+++ convergence-visualisation/simulations/moving_sim.py
@@ -0,0 +1,53 @@
+from matplotlib.animation import FuncAnimation, PillowWriter
+from scipy.spatial import Voronoi
+import os
+import numpy as np
+import random
+from src.entities import Quadcopter, PointOfInterest, AreaOfInterest
+from src.voronoi_utils import clipped_voronoi_polygons_2d
+from src.planner import assign_voronoi_targets
+from src.visualization import *
+
+def run(bounds=(160, 90), num_steps=200, seed=1):
+    # Set random seed for reproducibility
+    np.random.seed(seed)
+    random.seed(seed)
+
+    # Create results directory
+    results_dir = "results/moving_sim"
+    os.makedirs(results_dir, exist_ok=True)
+
+    # Define the values of the simulation
+    num_drones = 3
+    num_fw = 0
+    num_pois = 8
+
+    # Initialize drones, POIs, and AOIs
+    def random_point():
+        return (random.uniform(0, bounds[0]), random.uniform(0, bounds[1]))
+    drones = [Quadcopter(random_point()) for _ in range(num_drones)]
+    moving_percentage = 0.3  # 30% of POIs will move
+    pois = [
+        PointOfInterest(
+            random_point(),
+            weight=random.uniform(1.0, 10.0),
+            moving=random.random() < moving_percentage,
+            speed=random.uniform(1.0, 6.0)
+        )
+        for _ in range(num_pois)
+    ]
+    # print(pois)
+    aois = []
+
+    # Draw initial configuration
+    draw_static(drones, pois, aois, bounds, os.path.join(results_dir, "initial.png"))
+
+    # Set up the figure for animation
+    animate_simulation(drones, pois, aois, bounds,results_dir,  num_steps=num_steps, seed=seed)
+
+    # Draw final configuration
+    draw_static(drones, pois, aois, bounds, os.path.join(results_dir, "final.png"))
+
+    # Draw priority surface
+    draw_priority_surface(drones, pois, aois, bounds, os.path.join(results_dir, "priority_surface.png"))
+
