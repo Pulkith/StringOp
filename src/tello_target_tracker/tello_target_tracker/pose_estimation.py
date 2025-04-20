5ab1df0bc30676a6e03576fd656b50456a7145fd
Patrick Benito <peberhard@ethz.ch>
2025-04-20 05:44:18 -0700

    stuff

---
+++ src/tello_target_tracker/tello_target_tracker/pose_estimation.py
@@ -0,0 +1,63 @@
+import threading
+import time
+import pandas as pd
+
+class PoseEstimation:
+    def __init__(self, tello, x_init = 0):
+        # All drones are aligned at the beginning with an offset in x
+        self.tello = tello
+        self.x = x_init
+        self.y = 0
+        self.z = 0
+        self.yaw = 0
+
+        self.rate = 10
+        self.dt = 1 / self.rate
+        self.running = True
+        
+        self.previous_time = time.time()
+
+        self.thread = threading.Thread(target=self.loop, daemon=True)
+        self.thread.start()
+
+
+    def join(self):
+        print("Pose estimation data saved to pose_estimation.csv")
+        self.thread.join()
+        
+    def loop(self):
+        while self.running:
+            self.previous_time = time.time()
+            self.update()
+            time.sleep(self.dt)
+
+    def get_state(self):
+        # Get the current state of the drone
+        return {
+            "x": self.x,
+            "y": self.y,
+            "z": self.z,
+            "yaw": self.yaw,
+        }
+
+    def update(self):
+        try:
+            vx = self.tello.get_speed_x()
+            vy = self.tello.get_speed_y()
+            vz = self.tello.get_speed_z()
+
+            #print(f"Speed: vx={vx}, vy={vy}, vz={vz}")
+            #print(f"Acceleration: ax={ax}, ay={ay}, az={az}")
+            # y is the height of the drone
+            # z is the depth direction
+
+            self.x += vy * self.dt
+            self.z += vx * self.dt
+            #self.y = self.tello.get_height()
+            self.y += vz * self.dt
+            self.yaw = self.tello.get_yaw()
+
+            #print(f"Pose: x={self.x}, y={self.y}, z={self.z}, yaw={self.yaw}")
+            
+        except Exception as e:
+            print(f"Error: {e}")
--- test_swarm.py
+++ test_swarm.py
@@ -1,6 +1,9 @@
 from src.tello_target_tracker.tello_target_tracker.DJITelloPy.djitellopy import Tello, TelloSwarm
 import argparse
 
+from src.tello_target_tracker.tello_target_tracker.pose_estimation import PoseEstimation
+import time
+
 nets = [
     "wlp0s20f3",
     "wlx8c902de88812",
@@ -10,15 +13,30 @@ nets = [
 
 def test_swarm(net):
     tello = Tello(net=net)
-    tello.connect()
-    print(f"Battery: {tello.get_battery()}%")
+
+    try:
+        tello.connect()
+        print(f"Battery: {tello.get_battery()}%")
+        pose_estimation = PoseEstimation(tello, x_init=0)
+        tello.takeoff()
+        tello.move_up(100)
+        print("Moving up")
+        for i in range(10):
+            tello.move_forward(50)
+            tello.move_back(50)
+            tello.rotate_clockwise(90)
+            print("Moving forward")
+    finally:
+        tello.land()
+    
+        pose_estimation.join()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Test Tello Swarm")
     parser.add_argument(
         "--net",
         type=int,
-        default=1,
+        default=2,
         help="Network interface index to use for the Tello drones (default: 0)"
     )
     args = parser.parse_args()
